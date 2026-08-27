40963225fb939697509da5f07086701c98007615
saadkingtricks <saadkingtricks@gmail.com>
2026-08-27 22:17:33 +0500

    Add support for 'tw_live' in Thirdwave API authorization

--- saadotpbotmain.py
+++ saadotpbotmain.py
@@ -1237,7 +1237,7 @@ def panel_monitor_thread():
                             if "143.110.245.86" in str(urls_to_try):
                                 headers['Authorization'] = f'Bearer {token}'
                             # FIX: Add Authorization header for Thirdwave API
-                            if "thirdwave" in str(urls_to_try) or "app.thirdwave.im" in str(urls_to_try):
+                            if "thirdwave" in str(urls_to_try) or "app.thirdwave.im" in str(urls_to_try) or "tw_live" in str(urls_to_try):
                                 headers['Authorization'] = f'Bearer {token}'
                             for try_url in urls_to_try:
                                 try:
